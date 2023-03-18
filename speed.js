b9521a92c5057d9271f255a39ab43a74b41334cf
Nkatha_Glory <kinyua6945@gmail.com>
2023-03-18 17:05:11 +0300

    compeleted assignment

--- speed.js
+++ speed.js
@@ -1,11 +1,15 @@
 const speed = 80;
+//declare the speed limit 
             const speedLimit = 70;
             const kmPerDemeritPoint = 5;
             if (speed <= speedLimit) {
             console.log("Ok");
-            } else {
+            } //so if speed exceeds 70km/h it should output the demerit points
+            else {
              const demeritPoints = Math.floor((speed - speedLimit) / kmPerDemeritPoint);
              console.log(`Demerit points: ${demeritPoints}`);
-            }
+            }if (demeritPoints > 12) {
+                console.log("License suspended")
+            }//if the demerit points exceed 12 one should get the license suspended
             
   
