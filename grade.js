6e8669d7f5d9b8bcffc05e082adff2f1df1bc950
Nkatha_Glory <kinyua6945@gmail.com>
2023-03-18 11:11:29 +0300

    add grade.js, connect to index.html

--- grade.js
+++ grade.js
@@ -1,26 +1,21 @@
-function grades(event) {
-    event.preventDefault()
-    const gradeElm =document
-    const score = parseInt(document.getElementById('grades').value)
-if (score >79 && score <=100) {
-    const gradeElm.innerhtml="A"
-    return "A"
-}
+//This is to show that if you input a score it should give a grade in return
 
-else if (score >= 60 &&  score < 80) {
-    const gradeElm.innerhtml="B"
-    return "B"
-}
-else if (score >= 49 && score ) {
-    const gradeElm.innerhtml="C"
-    return "C"
-}
-else if (score >=40 && score < 50) {
-    const gradeElm.innerhtml="D"
-    return "D"
-}
-else if (score < 40 && score <= 0) {
-    const gradeElm.innerhtml="E"
-    return "E"
-}
-}
+if (score > 79 && score <= 100) {
+    console.log("A");
+}//if it fulfills the condition it should give the grade A
+else if(score >= 60 && score < 80) {
+    console.log("B")
+}//if not it does this instead
+else if(score >= 49 && score < 60) {
+    console.log("C");
+}//if it still doesn't it should output this 
+else if(score >= 40 && score < 50) {
+    console.log("D");
+}//it still doesn't this should be done instead
+else if (score < 49 && score >= 0){
+    console.log("E");
+}//if it doesn't it should do this instead
+
+else {
+  console.log("Invalid!!, Input correct Marks");
+}//if the score is not within the desired range then this shoul be the outputs
